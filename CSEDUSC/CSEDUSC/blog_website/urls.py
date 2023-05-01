396df0ea8969bf1daa5bc52782e077cbb909e0d5
Mahnaz <mahnazameer123@gmail.com>
2023-05-01 20:44:41 +0600

    anywhere

--- CSEDUSC/CSEDUSC/blog_website/urls.py
+++ CSEDUSC/CSEDUSC/blog_website/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('', include('user_profile.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
